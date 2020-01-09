26af7a9ee27b103e349181e73c0d1b35f21f0432
Alexey Ermakov <alexey.ermakov@zalando.de>
2020-01-09 13:56:15 +0100

    NodePool: add AvailableStorage()
    Signed-off-by: Alexey Ermakov <alexey.ermakov@zalando.de>

--- api/node_pool_test.go
+++ api/node_pool_test.go
@@ -3,6 +3,8 @@ package api
 import (
 	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestSortNodePools(t *testing.T) {
@@ -54,6 +56,39 @@ func TestSortNodePools(t *testing.T) {
 			if tc.pools[0].Name != tc.first {
 				t.Errorf("expected Node Pool '%s' to be sorted first", tc.first)
 			}
+		})
+	}
+}
+
+func TestAvailableStorage(t *testing.T) {
+	ebsSize := int64(10 * 1024 * 1024)
+
+	for _, tc := range []struct {
+		msg           string
+		instanceTypes []string
+		expected      int64
+	}{
+		{
+			msg:           "no instance storage",
+			instanceTypes: []string{"m5.xlarge"},
+			expected:      ebsSize,
+		},
+		{
+			msg:           "mixed with/without instance storage",
+			instanceTypes: []string{"m5.xlarge", "m5d.xlarge"},
+			expected:      ebsSize,
+		},
+		{
+			msg:           "nodes with instance storage",
+			instanceTypes: []string{"m5d.8xlarge", "m5d.24xlarge"},
+			expected:      0.75 * 2 * 600 * 1024 * 1024 * 1024,
+		},
+	} {
+		t.Run(tc.msg, func(t *testing.T) {
+			pool := NodePool{InstanceTypes: tc.instanceTypes}
+			size, err := pool.AvailableStorage(ebsSize, 0.75)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, size)
 		})
 	}
 }
