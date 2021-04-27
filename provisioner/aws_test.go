c85c787b1137e6d6b6d056a2833249a24106f1b9
rreis <rodrigo@zalando.de>
2021-04-27 21:41:08 +0200

    Unfinished test.
    Signed-off-by: rreis <rodrigo@zalando.de>

--- provisioner/aws_test.go
+++ provisioner/aws_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/aws/aws-sdk-go/service/cloudformation/cloudformationiface"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/aws/aws-sdk-go/service/kms/kmsiface"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -26,6 +27,20 @@ import (
 
 	log "github.com/sirupsen/logrus"
 )
+
+type ec2APIStub struct{}
+
+func (e *ec2APIStub) DescribeVpcs(*ec2.DescribeVpcsInput) (
+	*ec2.DescribeVpcsOutput,
+	error,
+) {
+	return &ec2.DescribeVpcsOutput{
+		Vpcs: []*ec2.Vpc{&ec2.Vpc{IsDefault: aws.Bool(false)}},
+	}, nil
+}
+
+func (e *ec2APIStub) AcceptReservedInstancesiExchangeQuote(*ec2.DescribeVpcsInput) (
+}
 
 type s3APIStub struct{}
 
@@ -157,6 +172,7 @@ func newAWSAdapterWithStubs(status string, groupName string) *awsAdapter {
 		session:              &session.Session{Config: &aws.Config{Region: aws.String("")}},
 		cloudformationClient: &cloudFormationAPIStub{statusMutex: &sync.Mutex{}, status: aws.String(status)},
 		s3Client:             &s3APIStub{},
+		ec2Client:            &ec2APIStub{},
 		autoscalingClient:    &autoscalingAPIStub{groupName: groupName},
 		apiServer:            "",
 		dryRun:               false,
