a1893617483639998d71d817c579f4f9379eaa13
Mahmoud Gaballah <mahmoud.gaballah@zalando.de>
2022-10-10 14:36:57 +0200

    increase test coverage
    Signed-off-by: Mahmoud Gaballah <mahmoud.gaballah@zalando.de>

--- api/node_pool_test.go
+++ api/node_pool_test.go
@@ -10,3 +10,13 @@ func TestIsSpotIO(t *testing.T) {
 	pool := NodePool{Profile: "worker-karpenter"}
 	require.True(t, pool.IsKarpenter())
 }
+
+func TestTaints(t *testing.T) {
+	pool := NodePool{ConfigItems: map[string]string{
+		"taints": "dedicated=test:NoSchedule,example:NoSchedule",
+	}}
+	require.Equal(t, []Taint{
+		{Key: "dedicated", Value: "test", Effect: "NoSchedule"},
+		{Key: "example", Value: "", Effect: "NoSchedule"},
+	}, pool.Taints())
+}
